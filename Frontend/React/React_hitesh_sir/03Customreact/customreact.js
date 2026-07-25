05ff21cc990c2cc11bdef6c91a0ec391942b0d02
sahil padaliya <sahilpadaliya36@gmail.com>
2026-07-25 12:13:22 +0530

    Custom React Library

---
+++ Frontend/React/React_hitesh_sir/03Customreact/customreact.js
@@ -0,0 +1,32 @@
+ function customrender(reactElement , container){
+   //  const domelement = document.createElement(reactElement.type);
+   //  domelement.innerHTML= reactElement.children
+   //  domelement.setAttribute('href',reactElement.props.href)
+   //  domelement.setAttribute('target',reactElement.props.target)
+
+   //  container.appendChild(domelement)
+
+
+   // now using the loop 
+   const domelement = document.createElement(reactElement.type);
+   domelement.innerHTML=  reactElement.children;
+   for (const prop in reactElement.props) {
+     if(prop ==='children')continue;
+     domelement.setAttribute(prop,reactElement.props[prop])
+   }
+   container.appendChild(domelement);
+
+
+ }
+ const reactElement = {
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children:'Click here to visit to google',
+ }
+ 
+ 
+ const maincontainer = document.querySelector('#root')
+ customrender(reactElement, maincontainer);
