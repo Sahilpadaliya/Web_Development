cf6636e8b5ff4f0e3311d8007c6a6024a194f15f
sahil padaliya <sahilpadaliya36@gmail.com>
2026-07-25 08:24:13 +0530

    Head Tag and Seo

---
+++ Frontend/Html/lovebabbar_Youtube_html/Lecture9/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function(){
+    alert('hi i m using the javascript in my local system')
+})
+
+const button = document.querySelector('#button')
+button.addEventListener('click',function(){
+    alert('hare krishna')
+    console.log('tt')
+})
